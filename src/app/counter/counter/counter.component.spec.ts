0dc8a5db5e4bcebfb679ffa9f34617c536eaa7b1
manuilenkoart <manuilenkoart@gmail.com>
2021-03-14 00:09:15 +0200

    тестирование EventEmitter

--- src/app/counter/counter/counter.component.spec.ts
+++ src/app/counter/counter/counter.component.spec.ts
@@ -1,27 +1,37 @@
 import { CounterComponent } from './counter.component';
 
-describe('CounterComponent', ()=>{
-let component: CounterComponent
-beforeEach(()=>{
+describe('CounterComponent', () => {
+let component: CounterComponent;
+
+beforeEach(() => {
   component = new CounterComponent();
-})
+});
 
 
   // xit - прорустить тест
-  //deforeEach - для каждого it
-  //beforeAll - перед всеми it
-  //afterEach - после завершения каждого it
-  //afterAll - после завершения всех тестов
+  // beforeEach - для каждого it
+  // beforeAll - перед всеми it
+  // afterEach - после завершения каждого it
+  // afterAll - после завершения всех тестов
 
 
-  it('should increment counter +1', ()=>{
+it('should increment counter +1', () => {
     component.increment();
-    expect(component.counter).toBe(1)
+    expect(component.counter).toBe(1);
   });
 
-it('should decrement counter -1', ()=>{
+it('should decrement counter -1', () => {
   component.decrement();
-  expect(component.counter).toBe(-1)
-})
-})
+  expect(component.counter).toBe(-1);
+});
+
+it('should increment value by event emitter', () => {
+  let result = null;
+  component.counterEmitter.subscribe(value => result = value);
+  component.increment();
+  // @ts-ignore
+  expect(result).toBe(1);
+});
+
+});
 
--- src/app/counter/counter/counter.component.ts
+++ src/app/counter/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,8 +7,12 @@ template: `Counter {{counter}}`
 export class CounterComponent {
 counter = 0 ;
 
+@Output() counterEmitter = new EventEmitter<number>();
+
+// tslint:disable-next-line:typedef
 increment(){
   this.counter ++;
+  this.counterEmitter.emit(this.counter);
 }
 decrement(){
   this.counter --;
